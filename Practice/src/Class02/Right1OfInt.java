d857013b5d0a98e85b478b02411949ace2be8253
SeaSoonKeun <1123442688@qq.com>
2021-05-16 15:53:33 +0800

    二分，异或

--- Practice/src/Class02/Right1OfInt.java
+++ Practice/src/Class02/Right1OfInt.java
@@ -11,8 +11,12 @@ package Class02;
 * 解题思路：
 * 1，取反加一 刚好是 最后一位为0，前面的数前面位数相反，从右往左最后一个为1的数，就是该值最右侧的一
 * 2，取与，只有最后一位的地方是1
-a & (a + 1) 反码加一
-a & (-a)
+a & (~a + 1) 反码加一
+a & (-a) 负数等去取反加一
 */
 public class Right1OfInt {
+    public static void main(String[] args) {
+
+    }
+    public static int
 }
