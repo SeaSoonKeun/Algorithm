afa91a69d2bf28b1910a598b93819843cb7d97b2
SeaSoonKeun <1123442688@qq.com>
2021-05-18 11:20:33 +0800

    二分法的解决边界问题

---
+++ Practice/src/Class01/RightBiggerOfSortedArray.java
@@ -0,0 +1,92 @@
+package Class01;
+
+import java.util.Arrays;
+
+/**
+ * @Auther: xucg
+ * @Date: 2021/5/17 - 05 - 17 - 上午8:45
+ * @Description: Class01
+ */
+public class RightBiggerOfSortedArray {
+    
+    public static void main(String[] args) {
+        int testTime = 30;
+        int maxSize = 10;
+        int maxValue = 20;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize,maxValue);
+            Arrays.sort(arr1);
+            printArray(arr1);
+            int[] arr2 = copyArray(arr1);
+//            System.out.println(rigthBiggerOfSortedArray(arr1, 3) == comparator(arr2, 3) ?
+//                    "大于等于3最左侧的位置索引是：" + rigthBiggerOfSortedArray(arr1, 3) : "出错了");
+            System.out.println(rigthBiggerOfSortedArray(arr1, 3) == comparator(arr2, 3) ?
+                    "success" : "fail");
+        }
+    }
+    public static int rigthBiggerOfSortedArray(int[] arr, int num){
+        if (arr.length == 0){
+            return -1;
+        }else{
+            int L = 0;
+            int R = arr.length - 1;
+            int Mid = 0;
+            // 引入index
+            int index = -1;
+            if(arr[L] > num){
+                return -1;
+            }
+            while (L <= R){
+                Mid = L + ((R - L) >> 1);
+                if (arr[Mid] <= num){
+                    index = Mid;
+                    L = Mid + 1;
+                }else {
+                    R = Mid - 1;
+                }
+            }
+            return index;
+        }
+    }
+    public static int[] generateRandomArray(int maxSize, int maxValue){
+        int length = (int)((maxSize + 1) * Math.random());
+        int[] arr = new int[length];
+        for (int i = 0; i < arr.length; i++) {
+            int value = (int)((maxValue + 1) * Math.random()) - (int)((maxValue + 1) * Math.random());
+            arr[i] = value;
+        }
+        return arr;
+    }
+    public static int[] copyArray(int[] arr){
+        if (arr.length == 0){
+            System.out.println("数组为空");
+            return null;
+        }
+        int[] arr1 = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr1[i] = arr[i];
+        }
+        return arr1;
+    }
+    public static void printArray(int[] arr ){
+        if (arr.length == 0){
+            System.out.println("数组为空");
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static int comparator(int[] arr, int num){
+        if(arr == null || arr.length == 0){
+            return -1;
+        }
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if(arr[i] <= num){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
